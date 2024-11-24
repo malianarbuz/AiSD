953c621f793fa4c2856126fc0c826ac0ec71913c
malianarbuz <147510893+malianarbuz@users.noreply.github.com>
2024-11-24 22:35:08 +0100

    a

---
+++ Grafy/NodeG.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grafy
+{
+    internal class NodeG
+    {
+        List<NodeG> neighbors;
+        public int data;
+
+        public NodeG(int data)
+        {
+            this.data = data;
+        }
+
+        public void AddConnection(NodeG node)
+        {
+            if(this.neighbors.Contains(node))
+                return;
+            this.neighbors.Add(node);
+            node.neighbors.Add(this);
+        }
+    }
+}
